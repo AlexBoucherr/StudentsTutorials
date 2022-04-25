6fae32483e5971bc92acbd1e2c4f21241157c948
AlexBoucherr <84985949+AlexBoucherr@users.noreply.github.com>
2022-04-25 15:57:15 +0100

    Add files via upload

--- slabsCaNH.py
+++ slabsCaNH.py
@@ -7,29 +7,33 @@ import ase.io.vasp
 canh = read('/home/alexbou/CONTCAR_CaNH_isif')
 slab = surface(canh, (0, 0, 1), layers=2, vacuum=5, periodic=True) * (2, 2, 1)
 
-hydrogen_atoms = []
+n_atoms = []
 for atom in slab:
-    if atom.symbol == 'H' and round(atom.position[2], 3) < 5.500:
-        hydrogen_atoms.append(atom.index)
+    if atom.symbol == 'N' and round(atom.position[2], 3) == 15.425:
+        n_atoms.append(atom.index)
+view(slab)
 
 for atom in slab:
-    if atom.index in hydrogen_atoms:
-        atom.position[2] += 10.500
+    if atom.index in n_atoms:
+        atom.position[2] = 4.660
+        atom.position[0] += 1.296
+view(slab)
 
 min_heigh = 100
 for atom in slab:
     if atom.position[2] < min_heigh:
         min_heigh = atom.position[2]
+view(slab)
 
 for atom in slab:
     atom.position[2] = atom.position[2] - min_heigh
 
 frozen_atoms = []
 for atom in slab:
-    if round(atom.position[2], 3) <= 5.383:
+    if round(atom.position[2], 3) <= 3.464:
         frozen_atoms.append(atom.index)
 
 slab.set_constraint(FixAtoms(indices=frozen_atoms))
 view(slab)
 
-ase.io.vasp.write_vasp('/home/alexbou/POSCAR_CaNH001', slab, vasp5=True, sort=True, direct=True)
+ase.io.vasp.write_vasp('/home/alexbou/POSCAR_CaNH001_v2', slab, vasp5=True, sort=True, direct=True)
